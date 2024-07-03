7d4f164755472b3f96e633633f7c9abee66e5dab
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2024-07-04 00:17:48 +0300

    Add MVC and BL layers
    Changed Microsoft.EntityFrameworkCore.Design package version 8.0.6 to version 8.0.5
    Added connection string to appsettings.json

---
+++ SocialMediaWebsite.MVC/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaWebsite.Core.BusinessLogic;
+using SocialMediaWebsite.Entities.DbContexts;
+
+namespace SocialMediaWebsite.MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(p => p.UseMySQL(builder.Configuration.GetConnectionString("SocialMediaWebsite")));
+
+            builder.Services.AddScoped(typeof(IManager<,>), typeof(Manager<,>));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
