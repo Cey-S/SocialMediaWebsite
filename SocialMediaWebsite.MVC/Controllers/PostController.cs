69e57f994487baa358fc510cbb36780da4d0a217
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2024-07-09 01:05:53 +0300

    Add Post Controller and Create View

---
+++ SocialMediaWebsite.MVC/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMediaWebsite.BLL.Abstract;
+using SocialMediaWebsite.MVC.Models;
+
+namespace SocialMediaWebsite.MVC.Controllers
+{
+	public class PostController : Controller
+	{
+		private readonly IPostManager postManager;
+
+		public PostController(IPostManager postManager)
+        {
+			this.postManager = postManager;
+		}
+
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+
+		[HttpGet]
+        public IActionResult Create()
+		{
+			PostCreateVM vM = new PostCreateVM();
+			vM.Username = "Username";
+			vM.ProfilePicture = "../img/logo/hashtag_128.png";
+
+			return View(vM);
+		}
+
+		//[HttpPost]
+		//public IActionResult Create()
+		//{
+
+		//	return View();
+		//}
+	}
+}
---
+++ SocialMediaWebsite.MVC/Models/PostCreateVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SocialMediaWebsite.MVC.Models
+{
+	public class PostCreateVM
+	{
+        public string Username { get; set; }
+        public string ProfilePicture { get; set; }
+
+        [MaxLength(150, ErrorMessage = "The title can't be longer than 150 characters")]
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The title can't be empty")]
+		public string Title { get; set; }
+
+		[MaxLength(4000, ErrorMessage = "The body text can't be longer than 4000 characters")]
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The body text can't be empty")]
+		public string Body { get; set; }
+
+        public List<string> TagNames { get; set; }
+    }
+}
