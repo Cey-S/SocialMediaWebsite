d655bcefd6ed35f8aa50a115dd80d09e4971a84f
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2024-07-25 22:57:06 +0300

    Add Role-based Authorization to PostController
    The Post controller redirects the user to the login page if the user is not logged in.

--- SocialMediaWebsite.MVC/Controllers/PostController.cs
+++ SocialMediaWebsite.MVC/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SocialMediaWebsite.BLL.Abstract;
@@ -10,6 +11,7 @@ using SocialMediaWebsite.MVC.Models;
 
 namespace SocialMediaWebsite.MVC.Controllers
 {
+	[Authorize(Roles = "AppUser")]
 	public class PostController : Controller
 	{
 		private readonly IPostManager postManager;
