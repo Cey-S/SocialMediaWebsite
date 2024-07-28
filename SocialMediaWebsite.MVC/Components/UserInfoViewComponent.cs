9fe4d5d8f0e8dff612d68c114c820ace039d2c56
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2024-07-28 18:49:42 +0300

    Add User Info View Component
    User info is shown in the dropdown menu of the layout.

---
+++ SocialMediaWebsite.MVC/Components/UserInfoViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaWebsite.Core.Entities;
+using SocialMediaWebsite.MVC.Models;
+
+namespace SocialMediaWebsite.MVC.Components
+{
+	public class UserInfoViewComponent(UserManager<MyUser> userManager) : ViewComponent
+	{
+		private readonly UserManager<MyUser> userManager = userManager;
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var user = await userManager.FindByNameAsync(User.Identity.Name);
+			var userRoles = await userManager.GetRolesAsync(user);
+			UserInfoVM vm = new UserInfoVM() { UserName = user.UserName, Role = userRoles.FirstOrDefault() };
+			return View(vm);
+		}
+	}
+}
---
+++ SocialMediaWebsite.MVC/Models/UserInfoVM.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMediaWebsite.MVC.Models
+{
+	public class UserInfoVM
+	{
+        public string UserName { get; set; }
+        public string Role { get; set; }
+    }
+}
